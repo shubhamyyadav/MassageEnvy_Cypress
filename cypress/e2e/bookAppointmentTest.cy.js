dab481f933e2c1f763b90f8ab15f27fce82fdb75
shubhamyyadav <shubhamyyadav@deloitte.com>
2023-03-22 15:52:21 +0530

    Wed 22 Mar, 3:52PM

--- cypress/e2e/bookAppointmentTest.cy.js
+++ cypress/e2e/bookAppointmentTest.cy.js
@@ -4,6 +4,7 @@ describe('E2E Appointment Booking', function () {
 
     it('Navigate to Website and Book an Appointment', function () {
         appointment.endToEndBooking();
+        console.log("The execution of Testcase is Complete.");
     });
 
 });
